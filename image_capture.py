c3a938b16eba5aeccca5643bb140b64152d7ae8b
ethjlee <153250364+ethjlee@users.noreply.github.com>
2024-04-01 15:55:37 -0700

    added infinite ability

--- image_capture.py
+++ image_capture.py
@@ -47,7 +47,8 @@ class Browser():
             self.driver.maximize_window()
             self.driver.get(home_link)
             if self.driver.title:
-                print(f"Web driver initialized and navigated to home: {home_link}.")
+                #print(f"Web driver initialized and navigated to home: {home_link}.")
+                pass
             else:
                 raise NoDriverFunctionality(home_link) 
         except Exception as e:
@@ -294,11 +295,27 @@ def get_credentials(os, admin_name="", admin=False):
 
     return (username, password)
         
+def capture(batches, batch_size, country, path):
+    for i in tqdm(range(batches), desc="Total batch progress", miniters=0):
+        data_acq = Browser(username, password, path)
+        data_acq.start_game(country=country, num_images=batch_size)
+        data_acq.driver.close()
 
 if __name__ == "__main__":
     # path to GG credentials is located in default "home folder/admin_name"
     username, password = get_credentials(os="Mac", admin_name="ethan", admin=True)
-    # change save path to PARENT GG folder (sub-dirs created for ea. country)
-    data_acq = Browser(username, password, "/Users/ethan/Documents/GeoGuessrAI")
+    
+    overnight = True
+    batches = 40
+    batch_size = 500
+    country = "taiwan"
+    save_path = "/Users/ethan/Documents/GeoGuessrAI" # change save path to PARENT GG folder (sub-dirs created for ea. country)
 
-    data_acq.start_game(country="andorra", num_images=8400)
+    if overnight:
+        while overnight:
+            try:
+                capture(batches, batch_size, country, save_path)
+            except:
+                pass
+    else:
+        capture(batches, batch_size, country, save_path)
--- image_processing.py
+++ image_processing.py
@@ -5,6 +5,7 @@ import os
 def convert_png_to_jpg(path):
     image_names = os.listdir(path)
     image_names = [name for name in image_names if ".png" in name]
+    image_names = [name for name in image_names if not name.split(".png")[0]+".jpg" in os.listdir(path)]
     for i in tqdm(range(len(image_names)), desc="PNG to JPG conversion progress"):
         name = image_names[i]
         image = Image.open(f"{path}/{name}")
@@ -15,21 +16,45 @@ def resize():
     pass
 def get_folder_size(folder_path):
     total_size = 0
-    
-    # Walk through all files and subdirectories in the folder
-    for dirpath, _, filenames in os.walk(folder_path):
-        # Add the size of each file to the total size
-        for filename in filenames:
-            file_path = os.path.join(dirpath, filename)
-            total_size += os.path.getsize(file_path)
-    
+    png_size = 0
+    jpg_size = 0
+    num_pngs = 0
+    num_jpgs = 0
+    for filename in os.listdir(folder_path):
+        # Construct the full file path
+        file_path = os.path.join(folder_path, filename)
+        
+        # Check if the file is a PNG file
+        if filename.endswith('.png'):
+            # Get the size of the PNG file
+            size = os.path.getsize(file_path)
+            total_size += size
+            png_size += size
+            num_pngs += 1
+        
+        # Check if the file is a JPG file
+        elif filename.endswith('.jpg'):
+            # Get the size of the JPG file
+            size = os.path.getsize(file_path)
+            total_size += size
+            jpg_size += size
+            num_jpgs += 1
+
     # Convert the total size to gigabytes
-    total_size_gb = total_size / (1024**3)
+    gb_converter = 1024**3
+    total_size_gb = total_size / gb_converter
+    png_size_gb = png_size / gb_converter
+    jpg_size_gb = jpg_size / gb_converter
     
-    return total_size_gb
+    return total_size_gb, png_size_gb, jpg_size_gb, num_pngs, num_jpgs
 
 if __name__ == "__main__":
     # path to images
-    folder = "/Users/ethan/Documents/GeoGuessrAI/andorra"
+    folder = "/Users/ethan/Documents/GeoGuessrAI/taiwan"
     convert_png_to_jpg(folder)
-    print("Folder size:", get_folder_size(folder), "GB")
+    t, p, j, np, nj = get_folder_size(folder)
+    print(f"Folder size: {t} GB")
+    print(f"Total size of PNGs: {p} GB")
+    print(f"Total size of JPGs: {j} GB")
+    print(f"Total number of PNGs: {np}")
+    print(f"Total number of JPGs: {nj}")
