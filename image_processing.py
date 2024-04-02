039ee7d57006032fbd8d2b72c738eac7d735893a
ethjlee <153250364+ethjlee@users.noreply.github.com>
2024-04-01 17:18:48 -0700

    added windows compatibility to png -> jpg convert

--- image_processing.py
+++ image_processing.py
@@ -9,9 +9,10 @@ def convert_png_to_jpg(path):
     image_names = [name for name in image_names if not name.split(".png")[0]+".jpg" in os.listdir(path)]
     for i in tqdm(range(len(image_names)), desc="PNG to JPG conversion progress"):
         name = image_names[i]
-        image = Image.open(f"{path}/{name}")
+        separator = "/" if "/" in path else "\\"                
+        image = Image.open(f"{path}{separator}{name}")
         rgb_image = image.convert('RGB')
-        rgb_image.save(path+"/"+name.split(".png")[0]+".jpg")
+        rgb_image.save(path+{separator}+name.split(".png")[0]+".jpg")
     
 def resize():
     pass
