c88d36cf1b0b9bf3b7c84463646283a7d2492b8a
AidanSS <aidansunahara@gmail.com>
2024-04-08 23:51:37 -0700

    ADD: resnet / dataset

--- dataset.py
+++ dataset.py
@@ -6,8 +6,9 @@ def main(countries_list):
     names = []
     cl = []
     i = 0
-    for root, dirs, files in os.walk(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), topdown=False):
+    for root, dirs, files in os.walk(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data"), topdown=False):
         for folder in dirs:
+            
             if folder in countries_list:
                 f = os.path.join(root, folder)
                 imgs = [im for im in os.listdir(f) if im.endswith(".jpg")] # python list
@@ -21,9 +22,16 @@ def main(countries_list):
     return df
 if __name__ == "__main__":
     dim = 224
-    countries = ["andorra", "taiwan", "south-korea"] # taiwan may not be a "country"!!!
+    countries = ["andorra", "taiwan", "south-korea", "malta"] # taiwan may not be a "country"!!!
     for i in range(len(countries)):
         countries[i] = f"{countries[i]}{dim}x{dim}"
-
-    main(countries_list=countries)
+    
+    df = main(countries_list=countries)
+    
+    
+    current_directory = os.getcwd()
+    parent_directory = os.path.dirname(current_directory)
+    file_path = os.path.join(parent_directory, "output.csv")
+    # Save the DataFrame to a CSV file
+    df.to_csv(file_path, index=False)  # Set index=False to exclude the index from the CSV file
 
